3beed173c9b8939b9fbff2e60b42c8d501a597f1
dada-mhmd <moudi.ld@hotmail.com>
2021-10-18 18:56:54 +0300

    project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand"></nav>
+    </div>
+  );
+}
+
+export default App;
